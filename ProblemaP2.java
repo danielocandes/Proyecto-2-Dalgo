67a14355ff78968f4c92636556f6d92834eb0f3b
danielocandes <d.ocampol@uniandes.edu.co>
2025-11-06 23:44:22 -0500

    ok ahora si creo tuve que quitar unos imports equisde. estoy en mis rodillas llorando y rezando que ahora si todo este bien y que me bendigan con el informe.

--- ProblemaP2.java
+++ ProblemaP2.java
@@ -1,11 +1,9 @@
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
-import java.util.Scanner;
 import java.util.Set;
 
 class Partition{
